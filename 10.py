ed103d3fc2ed9c0a59613cc82a62f8673e1792b8
Cristofer Alan Navarro Gomes <100239536+cristofergomes@users.noreply.github.com>
2022-02-23 19:50:37 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 09:46:13 2022
+
+@author: Cris
+
+Project Euler 10
+"""
+soma = 2
+for i in range(3,2000000,2):
+    for j in range(2,i,1):
+        sobra = i%j
+        if sobra == 0:
+            break
+# A lógica que eu usei para a otimização foi a seguinte
+# Se você faz uma divisão x/y e obtem z, automaticamente vc obtem a resposta de x/z que é y
+# Então se o y for maior que x/y, então vc ja testou todas divisões para x 
+        if j > (i/j):
+            break
+    if sobra != 0  and j > (i/j):
+        soma = soma+i
+print("acabou")
+print(soma)
