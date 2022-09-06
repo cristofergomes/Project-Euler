2adfba129a6bee05a19624a17e85f21d4245317c
Cristofer Alan Navarro Gomes <100239536+cristofergomes@users.noreply.github.com>
2022-09-06 16:24:19 -0300

    Update 0010.py

--- 0010.py
+++ 0010.py
@@ -7,14 +7,15 @@ Created on Wed Feb 23 09:46:13 2022
 Project Euler 10
 """
 soma = 2
-for i in range(3,2000000,2): #tirando 2, todo numero par n é primo
+for i in range(3,2000000,2): #tirando 2, todo numero par não é primo
     for j in range(2,i,1):
         sobra = i%j
         if sobra == 0:
             break
-# A lógica que eu usei para a otimização foi a seguinte
-# Se você faz uma divisão x/y e obtem z, automaticamente vc obtem a resposta de x/z que é y
-# Então se o y for maior que x/y, então vc ja testou todas divisões para x 
+            
+# Quando você faz uma divisão x/y = z, automaticamente vc obtem a resposta de x/z que é y.
+# Então, no somatório em y de x/y=z, quando o y for maior que z e nenhum z é inteiro, significa que (em um x/z = y) não há nenhum z inteiro que dá um y inteiro.
+
         if j > (i/j):
             break
     if sobra != 0  and j > (i/j):
